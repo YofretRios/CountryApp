96c4752350eebe47f6236838644e555cbb400763
YofretRios <riosmerca28@gmail.com>
2021-01-25 22:40:50 -0500

    Adding small Scafold

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Countries Of The World</h1>
+          <h1 className="App-title">Start Clean Project</h1>
         </header>
+
+        <Card />
+        <Card />
       </div>
     );
   }
