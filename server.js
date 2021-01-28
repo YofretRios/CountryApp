35951b5c8c98922e2840adad3fba386556274502
YofretRios <riosmerca28@gmail.com>
2021-01-28 00:18:28 -0500

    fix: finalizing details

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
+
 app.listen(port, () => console.log(`App is live on port ${port}!`))
